ada292a0d156fb8e47ff90ed2cb4fe89f724e621
unknown <nilkanth8747@gmail.com>
2025-07-22 11:50:11 +0530

    add all files

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
+
+# Create sample data (optional - comment out if not needed)
+python manage.py create_sample_data --clear
